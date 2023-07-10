0af7eaded29bae1ae3c4510a3c0d5beb79afac94
RicercarG <yg2709@nyu.edu>
2023-07-10 15:37:59 +0800

    2 projects finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// randomly add decorations to the first page
 window.addEventListener('DOMContentLoaded', () => {
   const randomItems = document.querySelectorAll('.material-symbols-rounded');
   const container = document.querySelector('.name');
@@ -12,3 +13,33 @@ window.addEventListener('DOMContentLoaded', () => {
     item.style.top = `${top}px`;
   });
 });
+
+// Function to scroll to the top of the page
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+
+// Show/hide the button based on the scroll position
+window.onscroll = function() {
+  var scrollToTopButton = document.getElementById("scrollToTop");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+};
+
+
+// toggle expander
+function toggleExpander(expanderId) {
+  var content = document.getElementById("expander" + expanderId + "Content");
+  content.classList.toggle("show");
+  // if (content.style.display === "none") {
+  //   content.style.display = "block";
+  // } else {
+  //   content.style.display = "none";
+  // }
+}
